f4e452eb747e073287066b20a7796b00b04bec7e
yogesh8145 <venkatyogeshwar501@gmail.com>
2024-11-10 22:38:06 +0530

    Initial push

---
+++ src/ATBx06112024/HelloWorld.java
@@ -0,0 +1,16 @@
+package ATBx06112024;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("Hello World");
+//
+        /*
+
+
+
+
+        mnjknkni
+         */
+    }
+}
